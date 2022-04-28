544faf58c054fca63a6aaffaee2e7912466a54e3
Jordy Tejada <jtejadala@unsa.edu.pe>
2022-04-28 13:55:42 -0500

    Agregue mensaje de bienvenida al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
     public static void main(String args[]){
 	/*MI PRIMER LABORATORIO*/
 	System.out.println("¡Hola Mundo!");
+	System.out.println("¡Bienvenidos a Programacion Web 2!");
     }
 }
