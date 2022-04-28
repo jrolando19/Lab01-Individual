342923d234faa9eb037a42faa623a2465e0f16dd
Jordy Tejada <jtejadala@unsa.edu.pe>
2022-04-28 13:57:49 -0500

    Agregue detalles del programador

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo{
 	/*MI PRIMER LABORATORIO*/
 	System.out.println("¡Hola Mundo!");
 	System.out.println("¡Bienvenidos a Programacion Web 2!");
+	System.out.println("¡Att: Jordy Tejada!");
     }
 }
